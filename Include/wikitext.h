52ce79ac28e894ac6860d67cd182c0435fae012d
Andrew <tankist.scratch.p@gmail.com>
2021-08-08 15:06:28 +0300

    CMakeLists and init function

---
+++ Include/wikitext.h
@@ -0,0 +1,14 @@
+#ifndef WikiText_H
+#define WikiText_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int WikiText_Init(void);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* WikiText_H */
---
+++ src/init.c
@@ -0,0 +1,4 @@
+int WikiText_Init(void)
+{
+    return 0;
+}
